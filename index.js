f4d5a6aea09407c7d992a12b5951b0b24fc116b1
Bryan <thomas.bryan.m@gmail.com>
2020-10-11 19:09:21 -0700

    better filter

--- index.js
+++ index.js
@@ -4,36 +4,35 @@ const { terminologyDict } = require('./terminologyDict');
 
 //Execute Work Flow
 Toolkit.run(async tools => {
-    // Console logs to the actions dashboard
     //console.log(tools.context); //debug line
     if (!tools.context.payload.pull_request) { // event was not a pull request
         console.log('Unexpected event occurred. action context: ', tools.context.payload)
         tools.exit.neutral('Exited with unexpected event')
     }
 
-    // Pull Request details
-    let pathWork = tools.workspace;
-    console.log("workspace ", pathWork);
-    const contents = await tools.readFile('test.txt')
-    console.log("contents ", contents)
-    let bodyArr = contents.toLowerCase().split(/\s|\n|\r|,/g)
-    console.log("Array", bodyArr)
+    const fileContents = await tools.readFile('test.txt')
+    const fileContentsArr = fileContents.toLowerCase().split(/\s|\n|\r|,/g)
 
-    let errorFound = false;
-    let termsFound = [];
-    for (let word of bodyArr) {
+    let checkFailed = false;
+    let termsFound1 = [];
+    for (let word of fileContentsArr) {
         for (let term of terminologyDict) {
             if (word.includes(term)) {
-                errorFound = true;
-                termsFound.push({
+                checkFailed = true;
+                termsFound1.push({
                     "term found": term,
                     "wordFound": word
                 })
             }
         }
     }
+    let termsFound2 = fileContentsArr.filter(value => terminologyDict.includes(value));
+    console.log(`First Way:${JSON.stringify(termsFound1)}`)
+    console.log(`Second Way:${JSON.stringify(termsFound2)}`)
     //TODO Read all files
     // // Check if a term was found in the non inclusive dictionary
+    // let pathWork = tools.workspace;
+    // console.log("workspace ", pathWork);
     // let errorFound2 = false;
     // let termsFoundReader = [];
     // let files = fs.readdir(pathWork, (err, files) => { return files });
@@ -57,10 +56,10 @@ Toolkit.run(async tools => {
     //console.log("reader", JSON.stringify(termsFoundReader))
 
     // When a term is found post a comment on the PR
-    if (errorFound) {
+    if (checkFailed) {
         const commentMsg = `
       Please review your recent code change for non inclusive terms.\n
-      Terms Found: ${JSON.stringify(termsFound)}`;
+      Terms Found: ${JSON.stringify(termsFound1)}`;
         console.log(commentMsg);
         await tools.github.issues.createComment({
             owner: tools.context.payload.repository.owner.login,
