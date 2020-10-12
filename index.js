9647ce7e0267e7f3b9df2e3cd832107ff3848cb6
Bryan <thomas.bryan.m@gmail.com>
2020-10-11 19:12:45 -0700

    clean up

--- index.js
+++ index.js
@@ -14,21 +14,38 @@ Toolkit.run(async tools => {
     const fileContentsArr = fileContents.toLowerCase().split(/\s|\n|\r|,/g)
 
     let checkFailed = false;
-    let termsFound1 = [];
+    let termsFound = [];
     for (let word of fileContentsArr) {
         for (let term of terminologyDict) {
             if (word.includes(term)) {
                 checkFailed = true;
-                termsFound1.push({
+                termsFound.push({
                     "term found": term,
                     "wordFound": word
                 })
             }
         }
     }
+    //More efficient search?
     let termsFound2 = fileContentsArr.filter(value => terminologyDict.includes(value));
-    console.log(`First Way:${JSON.stringify(termsFound1)}`)
-    console.log(`Second Way:${JSON.stringify(termsFound2)}`)
+
+    // When a term is found post a comment on the PR
+    if (checkFailed) {
+        const commentMsg = `Please review your recent code change for non inclusive terms.\n
+          Terms Found: ${JSON.stringify(termsFound)}`;
+        console.log(commentMsg);
+        await tools.github.issues.createComment({
+            owner: tools.context.payload.repository.owner.login,
+            issue_number: tools.context.payload.pull_request.number,
+            repo: tools.context.payload.repository.name,
+            body: commentMsg
+        });
+        tools.exit.neutral('Exited with terms identified')
+    } else {
+        console.log("No non inclusive terminology was found in this pull request. Nice Job! :)");
+        tools.exit.success('Exited successfully')
+    }
+
     //TODO Read all files
     // // Check if a term was found in the non inclusive dictionary
     // let pathWork = tools.workspace;
@@ -52,24 +69,6 @@ Toolkit.run(async tools => {
     //         }
     //     }
     // }
-
     //console.log("reader", JSON.stringify(termsFoundReader))
 
-    // When a term is found post a comment on the PR
-    if (checkFailed) {
-        const commentMsg = `
-      Please review your recent code change for non inclusive terms.\n
-      Terms Found: ${JSON.stringify(termsFound1)}`;
-        console.log(commentMsg);
-        await tools.github.issues.createComment({
-            owner: tools.context.payload.repository.owner.login,
-            issue_number: tools.context.payload.pull_request.number,
-            repo: tools.context.payload.repository.name,
-            body: commentMsg
-        });
-        tools.exit.neutral('Exited with terms identified')
-    } else {
-        console.log("No non inclusive terminology was found in this pull request. Nice Job! :)");
-        tools.exit.success('Exited successfully')
-    }
 });
