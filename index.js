5bb4798a259c3deb1e905508821d09440e8ad175
Bryan <thomas.bryan.m@gmail.com>
2020-10-11 18:04:12 -0700

    update file stream

--- index.js
+++ index.js
@@ -17,20 +17,12 @@ Toolkit.run(async tools => {
     // Pull Request details
     pullRequestNum = tools.context.payload.pull_request.number
     let pathWork = tools.workspace;
-    console.log("workspace ", tools.workspace);
+    console.log("workspace ", pathWork);
     const contents = await tools.readFile('test.txt')
     console.log("contents ", contents)
     let bodyArr = contents.split(/\s|\n|\r|,/g)
     console.log("Array", bodyArr)
 
-    fs.readdir(pathWork, (err, files) => {
-        console.log("reading files", files)
-        files.forEach(file => {
-          console.log(file);
-        });
-      });
-
-    // Check if a term was found in the non inclusive dictionary
     let errorFound = false;
     let termsFound = [];
     for (let word of bodyArr) {
@@ -45,11 +37,35 @@ Toolkit.run(async tools => {
         }
     }
 
+
+    // Check if a term was found in the non inclusive dictionary
+    let errorFound2 = false;
+    let termsFoundReader = [];
+    let files = fs.readdir(pathWork, (err, files) => { return files });
+    for (file of files) {
+        console.log("File: ", file);
+        let contents2 = await tools.readFile(file)
+        bodyArr = contents2.split(/\s|\n|\r|,/g)
+        for (let word of bodyArr) {
+            for (let term of terminologyDict) {
+                if (word.includes(term)) {
+                    errorFound2 = true;
+                    termsFoundReader.push({
+                        "term found": term,
+                        "wordFound": word
+                    })
+                }
+            }
+        }
+    }
+
+    console.log("reader", JSON.stringify(termsFoundReader))
+
     // When a term is found post a comment on the PR
     if (errorFound) {
         const commentMsg = `
       Please review your recent code change for non inclusive terms.\n
-      Terms Found: ${termsFound}`;
+      Terms Found: ${JSON.stringify(termsFound)}`;
         console.log(commentMsg);
         await tools.github.issues.createComment({
             body: commentMsg
